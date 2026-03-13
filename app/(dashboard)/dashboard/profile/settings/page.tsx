b10de50f088e34629481afacb5d5fe76d18bef00
rakib3719 <bannah76769@gmail.com>
2026-03-13 11:34:32 +0600

    Refactor: Notification setting role by render

--- app/(dashboard)/dashboard/profile/settings/page.tsx
+++ app/(dashboard)/dashboard/profile/settings/page.tsx
@@ -1,9 +1,17 @@
 import NotificationPage from "@/components/shared/NotificationPage/NotificationPage";
+import { getCurrentUserFromTokenAction } from "@/services/auth";
+import { ReactNode } from "react";
 
-export default function UserProfileSettingsPage() {
+export default async function UserProfileSettingsPage({
+  children,
+}: {
+  children: ReactNode;
+}) {
+    const authState =await  getCurrentUserFromTokenAction();
+
   return (
     <div className='border border-[#D1CEC6] rounded-lg p-4 md:p-6'>
-      <NotificationPage />
+      <NotificationPage userRole={authState.userRole} />
     </div>
   );
 }
