0ec261e269270bc310520dec86fd36b7716e3b07
Ishrat2413 <ishratrintu2413@gmail.com>
2026-04-18 16:08:02 +0600

    feat: Add RealStories component and integrate into HomePage; create ReviewsPage with video fetching service

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -4,6 +4,7 @@ import DiscoverProperties from "@/components/home/Discover/DiscoverProperties";
 import HomeBanner from "@/components/home/HomeBanner";
 import MakeAJourney from "@/components/home/MakeAJourney";
 import NeedForHome from "@/components/home/NeedForHome";
+import RealStories from "@/components/home/real-stories";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 
 export default function HomePage() {
@@ -16,6 +17,7 @@ export default function HomePage() {
         <NeedForHome />
         <MakeAJourney />
         <WhyChooseUs />
+        <RealStories />
         <BlogArticles />
       </div>
     </div>
